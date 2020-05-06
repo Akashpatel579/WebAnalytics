e57b9bf3a0aba6786fe832cf121689a1308a6909
Akash Patel <akashpatel0509@gmail.com>
2020-05-06 19:27:54 -0400

    kafka produce messages

--- src/main/java/com/project/analytics/controller/HomeController.java
+++ src/main/java/com/project/analytics/controller/HomeController.java
@@ -1,6 +1,11 @@
 package com.project.analytics.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.analytics.model.ClickAnalytics;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,8 +14,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Properties;
+
 @Controller
 public class HomeController {
+
+	private final static String TOPIC = "click_events";
+	private final static String BOOTSTRAP_SERVERS = "localhost:9092";
 
 	@RequestMapping(value = "/", method = RequestMethod.GET )
 
@@ -22,9 +32,37 @@ public class HomeController {
 
 	@PostMapping(value = {"/clickAnalytics"}, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity storeClickEvent(@RequestBody ClickAnalytics clickAnalytics){
+
+		String clickEventData = clickAnalytics.getX() + "  " + clickAnalytics.getY();
 		System.out.println("Data logged! " + clickAnalytics.getX() + "  " + clickAnalytics.getY());
+
+		KafkaProducer<String, String> kafkaProducer = produce();
+
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+
+		kafkaProducer.send(new ProducerRecord(TOPIC, "Key", mapper.writeValueAsString(clickAnalytics)));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			kafkaProducer.close();
+		}
 		return ResponseEntity.ok().build();
 	}
 
 
+	public static KafkaProducer<String, String> produce() {
+		// Set properties used to configure the producer
+		Properties properties = new Properties();
+		// Set the brokers (bootstrap servers)
+		properties.setProperty("bootstrap.servers", BOOTSTRAP_SERVERS);
+		// Set how to serialize key/value pairs
+		properties.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		properties.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		// specify the protocol for Domain Joined clusters
+		//properties.setProperty(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
+
+		return new KafkaProducer<>(properties);
+
+	}
 }
